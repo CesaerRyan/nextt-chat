08988d2bd6bf7797f902196ed8e7bd41a82ea42b
unknown <412863158@qq.com>
2023-02-14 10:04:33 +0800

    WIP next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import SideBar from "@/components/SideBar";
 import "./globals.css";
+import { SessionProvider } from "@/components/SessionProvider";
 
-export default function RootLayout({
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Login from "@/components/Login";
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
       {/*
@@ -14,16 +21,18 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <div className="flex">
-          <div
-            className="bg-[#202223] max-w-xs h-screen overflow-y-auto
-          md:min-w-[20rem]
-          "
-          >
-            <SideBar></SideBar>
-          </div>
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202223] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] ">
+                <SideBar></SideBar>
+              </div>
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google'
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET_KEY!,
+            httpOptions: {
+                timeout: 100000
+            }
+        })
+    ]
+}
+
+export default NextAuth(authOptions)
