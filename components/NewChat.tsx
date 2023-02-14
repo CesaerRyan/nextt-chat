eac23e281fecb84be87347e5ffb4e558e2e84e74
unknown <412863158@qq.com>
2023-02-15 00:16:10 +0800

    create doc

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,10 +1,31 @@
+'use client'
+
 import { PlusIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import {useRouter} from 'next/navigation'
+import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import { db } from "@/firebase";
+
 
 function NewChat() {
+  const router = useRouter()
+  const {data:session} = useSession()
+  const createNewChat = async()=>{
+    const doc = await addDoc(collection(db,'users',session?.user?.email!,'chats'),{
+      userId: session?.user?.email!,
+      createdAt: serverTimestamp(),
+
+    })
+    router.push(`/chat/${doc.id}`)
+  };
+
+
+
+
   return (
     <div className="border-gra700 border chatRow">
       <PlusIcon className="h-4 w-4"></PlusIcon>
-      <p> 新聊天</p>
+      <p onClick={()=>createNewChat()}> 新聊天</p>
     </div>
   );
 }
