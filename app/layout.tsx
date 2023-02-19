3746296df12a8a09bf2b200e3212092b1d0aab69
unknown <412863158@qq.com>
2023-02-20 00:11:46 +0800

    firestore 存在问题， 少了messages的路径

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "@/components/SessionProvider";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Login from "@/components/Login";
+import ClientProvider from "@/components/ClientProvider";
 
 export default async function RootLayout({
   children,
@@ -29,6 +30,8 @@ export default async function RootLayout({
               <div className="bg-[#202223] max-w-xs h-screen overflow-y-auto md:min-w-[20rem] ">
                 <SideBar></SideBar>
               </div>
+
+              <ClientProvider />
               <div className="bg-[#343541] flex-1">{children}</div>
             </div>
           )}
--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,41 +1,95 @@
-'use client'
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid"
-import { useSession } from "next-auth/react"
-import { useState } from "react"
+"use client";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "@firebase/firestore";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
+import firestore from "firebase/firestore";
+import { Message } from "@/typing";
+import { db } from "@/firebase";
+import { Toaster, toast } from "react-hot-toast";
+type Props = {
+  chatId: string;
+};
 
-type Props ={
-    chatId:string
-}
-function ChatInput({chatId}:Props) {
-    const [prompt, setPrompt] = useState('')
-    const {data:session} = useSession()
-    return (
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  //TODO useSWR get Model
+  const model = "text-davinci-003";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    const notification = toast.loading(" ChatGPT 正在思考...");
+    await addDoc(
+      collection(db, "users", session?.user?.email!, "chats", "messages"),
+      message
+    );
+
+    await fetch("api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(() => {
+      // Toast notification to say successful
+      toast.success("ChatGPT 回复了", {
+        id: notification,
+      });
+    });
+  };
+
+  return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm  ">
-        
-        <form className="p-5 space-x-5 flex">
-            <input type="text"
-            className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
-            disabled={!session}
-            value={prompt}
-            onChange={e=>setPrompt(e.target.value)}
-            placeholder="输入问题"
-            />
-            <button type="submit"
-            
-            disabled={!session || !prompt} 
-            className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4  py-2 rounded
+      <form className="p-5 space-x-5 flex" onSubmit={sendMessage}>
+        <input
+          type="text"
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="输入问题"
+        />
+        <button
+          type="submit"
+          disabled={!session || !prompt}
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4  py-2 rounded
             disabled:bg-gray-300 disabled:cursor-not-allowed"
-            >
-                <PaperAirplaneIcon className="h-4 w-4 -rotate-45"></PaperAirplaneIcon>
-            </button>
-        </form>
+        >
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45"></PaperAirplaneIcon>
+        </button>
+      </form>
 
-
-        <div>
-            {/**Model Selection  */}
-        </div>
+      <div>{/**Model Selection  */}</div>
     </div>
-  )
+  );
 }
 
-export default ChatInput
+export default ChatInput;
